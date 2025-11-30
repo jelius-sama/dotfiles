c09ed9f8053b890ff01dcf272d8d784ade41f7a3
jelius-sama <jelius.basumatary.sama@gmail.com>
2025-11-30 10:27:48 +0530

    synced local changes

--- init.lua
+++ init.lua
@@ -237,6 +237,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*.c', '*.h', '*.cpp', '*.hpp', '*.cc', '*.hh' },
+  command = 'silent! %!clang-format',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
