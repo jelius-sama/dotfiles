6280fe46f4410b56a3b8e4670359a1566d8ca4e2
jelius-sama <jelius.basumatary.sama@gmail.com>
2025-12-17 12:53:49 +0530

    synced local changes

--- init.lua
+++ init.lua
@@ -1456,6 +1456,8 @@ require('lazy').setup({ -- NOTE: Plugins can be added with a link (or for a gith
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<Tab>'] = { 'fallback' },
+        ['<S-Tab>'] = { 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
